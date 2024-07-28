9a8b053be8690278f764827a0288de9294025dee
natachabkr1 <natachabkr@hotmail.com>
2024-07-28 15:42:20 -0300

    adicionado o desafio 2

---
+++ Desafio2/Exer_10.py
@@ -0,0 +1,1 @@
+# 10. Faça um programa que lê três números inteiros e os mostra em ordem crescente
---
+++ Desafio2/Exer_7.py
@@ -0,0 +1,1 @@
+# 7. Desenvolver um programa que solicite a idade do usuário e identifique se ele é uma criança, um adolescente, adulto ou idoso.

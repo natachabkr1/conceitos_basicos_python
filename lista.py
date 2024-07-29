9630498c948b0e63c6ffe49cede9dcf28bafcd21
natachabkr1 <natachabkr@hotmail.com>
2024-07-28 22:35:31 -0300

    adicionado conteúdo de listas, tuplas, dicionário e funções

---
+++ lista.py
@@ -0,0 +1,40 @@
+# O que são listas? salvar dados em uma lista
+
+'''lista_frutas = []  # declaração da lista
+
+lista_frutas.append('banana')  # adicionando um item à lista
+lista_frutas.append('maça')
+
+print(lista_frutas)  # imprimindo a lista'''
+
+'''lista_frutas[0] - retorna o primeiro valor da lista
+lista_frutas[-1] retorna o último item da lista
+lista_frutas.sort() - classifica a lista'''
+
+
+#pedindo para o usuário inserir um item na lista:
+
+'''lista_frutas = []  # declaração dalista
+fruta = input('Digite sua fruta: ')
+lista_frutas.append(fruta)
+
+print(lista_frutas)  # imprimindo a lista'''
+
+
+# Tuplas - Salvar os dados em uma lista sem eles poderem ser alterados
+
+'''tupla = ('maçã', 'banana', 'uva')
+print(tupla)'''
+
+
+# Dicionário - Qdo temos mais de um dado referente a um conjunto
+
+# dicionario = {'chave': 'valor'}
+
+dicionario = {}
+dicionario['maca'] = 'É uma fruta'
+dicionario['carro'] = 'É um veículo'
+dicionario['gato'] = 'É um animal'
+
+print(dicionario)
+
